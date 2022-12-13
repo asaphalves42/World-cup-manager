3cf1520f54d722297c2f754eadea5f6caf248ffd
Asaph Alves <1221473@isep.ipp.pt>
2022-12-13 21:54:09 +0000

    Classe equipa

---
+++ Equipas.java
@@ -0,0 +1,21 @@
+package equipes.TF.LP1;
+
+
+public class Equipas {
+	private String nome;
+	private String federacao;
+	private int dataInaug;
+	private int ranking;
+	private boolean presenteOuNao;
+	
+	public Equipas() {
+		this.nome="vazio";
+		this.federacao="vazio";
+		this.dataInaug=0;
+		this.ranking=0;
+		this.presenteOuNao=false;
+		
+	}
+	
+}
+
